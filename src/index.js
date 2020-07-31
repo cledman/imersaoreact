12d72b4e12df8744753eb3a5d42470086a0cd58d
Cledson Lopes <cledman@gmail.com>
2020-07-31 13:24:08 -0300

    Json Server created and form stylized

--- src/index.js
+++ src/index.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import CadastroVideo from './pages/register/Video'
-import ErrorPage from './pages/errors'
-import CadastroCategoria from './pages/register/Category';
+import Home from "./pages/Home";
+import CadastroVideo from "./pages/register/Video";
+import ErrorPage from "./pages/errors";
+import CadastroCategoria from "./pages/register/Category";
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      <Route path="/" component={Home}  exact/>      
-      <Route path="/register/video" component={CadastroVideo} />      
-      <Route path="/register/category" component={CadastroCategoria} />            
-      <Route component={ ErrorPage } />   
+      <Route path="/" component={Home} exact />
+      <Route path="/register/video" component={CadastroVideo} />
+      <Route path="/register/category" component={CadastroCategoria} />
+      <Route component={ErrorPage} />
     </Switch>
-    
-  </BrowserRouter>,  
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
-
--- src/pages/register/Category/index.js
+++ src/pages/register/Category/index.js
@@ -1,97 +1,90 @@
-import React, { useState } from 'react'
-import PageDefault from '../../../components/PageDefault'
-import { Link } from 'react-router-dom'
-import FormField from '../../../components/FormField'
+import React, { useState, useEffect } from "react";
+import PageDefault from "../../../components/PageDefault";
+import { Link } from "react-router-dom";
+import FormField from "../../../components/FormField";
+import Button from "../../../components/Button";
 
-const CadastroCategoria= () =>{
-
+const CadastroCategoria = () => {
   const initialValues = {
-    name:'',
-    description:'',
-    colour:''
+    name: "",
+    description: "",
+    colour: "",
+  };
+  const [categories, setCategories] = useState([]);
+  const [values, setValues] = useState(initialValues);
+
+  function setValue(key, value) {
+    setValues({
+      ...values,
+      [key]: value,
+    });
   }
-  const [categories, setCategories]= useState([])
-  const [values, setValues] = useState(initialValues)
 
+  const handleChange = (info) => {
+    setValue(info.target.getAttribute("name"), info.target.value);
+  };
 
+  const handleSubmit = (data) => {
+    data.preventDefault();
+    setCategories([...categories, values]);
 
-    function setValue(key, value){
-      setValues({
-        ...values,
-        [key]:value 
-      })
-    }
+    setValues(initialValues);
+  };
 
-    const handleChange = (info) =>{
-
-      setValue(
-        info.target.getAttribute('name'),
-        info.target.value
-        )
-    }
-    
-    const handleSubmit = (data) =>{
-      data.preventDefault()
+  useEffect(() => {
+    console.log("ioa");
+    const URL_CATEGORIES = "http://localhost:8080/categories";
+    fetch(URL_CATEGORIES)
+    .then(async(response) =>{
+      const resp = await response.json()
       setCategories([
-        ...categories,
-        values
-      ]) 
-      
-      setValues(initialValues)
-    }
+        ...resp,
+      ])
+    })
+  },[])
+  return (
+    <PageDefault>
+      <h1>Cadastro da Categoria {values.name}</h1>
 
-    return(
-      <PageDefault>
-          <h1>Cadastro da Categoria {values.name}</h1>
+      <form onSubmit={handleSubmit}>
+        <FormField
+          label="Nome da categoria"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
 
-        <form onSubmit={handleSubmit}>
+        <FormField
+          label="Descrição"
+          type="textarea"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
 
-          <FormField 
-            label="Nome da categoria"
-            type="text"
-            name="name"          
-            value = {values.name}
-            onChange = {handleChange}      
-           />
+        <FormField
+          label="Cor"
+          type="color"
+          name="colour"
+          value={values.colour}
+          onChange={handleChange}
+        />
 
-          <FormField 
-            label="Descrição:"
-            type="textarea"
-            name="description"
-            value = {values.colour}
-            onChange = {handleChange}      
-           />
+        <Button>Cadastrar</Button>
+      </form>
 
-          <FormField 
-            label="Cor"
-            type="color"
-            name="colour"
-            value = {values.colour}
-            onChange = {handleChange}      
-           />
+      {categories.length === 0 && <div>Loading...</div>}
 
+      <ul>
+        {categories.map((category) => {
+          return <li key={`${category.name}`}>{category.name}</li>;
+        })}
+      </ul>
 
+      <Link to="/">Ir para home</Link>
+    </PageDefault>
+  );
+};
 
-          <button>
-            Cadastrar
-          </button>
-        </form>
-
-        <ul>
-          {categories.map((category, key) =>{
-            return(
-              <li key={`${category}${key}`} >
-                {category.name}
-              </li>
-            )
-          })}
-        </ul>
-
-        <Link to="/">
-          Ir para home
-        </Link>
-      </PageDefault>
-    )
-  }
-  
-  export default CadastroCategoria
+export default CadastroCategoria;
